3f1c9adcf8e5a8abb2cce3c9c33a5d0bab809ff1
Buronov <boronovjonibek@gmail.com>
2020-06-29 18:27:37 +0300

    1-dars

---
+++ 1-dars/app.py
@@ -0,0 +1,10 @@
+fname = input('Enter your first name: ')
+lname = input('Enter your last name: ')
+age = str(input('Enter your age: '))
+id = str(input('Enter your student id: '))
+full_name = fname + " " +  lname
+
+print("*****************************************")
+print(f'Your name is {full_name}')
+print(f'Your age is {age}')
+print(f'Your id is {id}')
